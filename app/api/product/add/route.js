255572458302b2b55079cdc0cbf1718ea3068adc
najjarmalek <najjarmalek3@gmail.com>
2025-08-11 02:04:54 +0100

    creating product api's

---
+++ app/api/product/add/route.js
@@ -0,0 +1,71 @@
+import authSeller from "@/lib/authSeller"
+import { getAuth } from "@clerk/nextjs/server"
+import {v2 as cloudinary} from "cloudinary"
+import { NextResponse } from "next/server"
+
+cloudinary.config({
+    cloud_name:process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret:process.env.CLOUDINARY_API_SECRET 
+})
+
+export async function POST(request){
+    try {
+        const {userId}=getAuth()
+        const {isSeller}=await authSeller(userId)
+
+        if (! isSeller) {
+            return NextResponse.json({success:false,message:'not authorized'})
+        }
+
+        const formData = await request.formData()
+
+        const name = formData.get('name')
+        const description =formData.get('description')
+        const category =formData.get('category')
+        const price =formData.get('price')
+        const offerPrice =formData.get('offrePrice')
+
+        const files=formData.getAll('images')
+        if (!files || files.length ===0) {
+             return NextResponse.json({success:false,message:'no files uploaded'})
+        }
+        const result =await Promise.all(
+            files.map(async(file)=>{
+                const arrayBuffer=await file.arrayBuffer()
+                const buffer=Buffer.from(arrayBuffer)
+
+                return new Promise((resolve,reject)=>{
+                     const Stream = cloudinary.uploader.upload_stream(
+                        {resource_type:'auto'},
+                        (error,result)=>{
+                             if (error) {
+                                reject(error)
+                             }else{
+                                resolve(result)
+                             }
+                        }
+                     )
+                     Stream.end(buffer)
+                })
+            })
+        )
+
+        const image =result.map(result=>result.secure_url)
+        await connectDB()
+        const newProduct =await Product.create({
+            userId,
+            name,
+            description,
+            price:Number(price),
+            offerPrice:Number(offerPrice),
+            image,
+            category,
+            date:Date.now()
+        })
+
+        return NextResponse.json({success:true,message:'upload successful',newProduct})        
+    } catch (error) {
+        return NextResponse.json({success:false,message:error.message})
+    }
+}
