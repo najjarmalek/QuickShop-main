2396ba5e14024e369553f2d27855c9a3929c4b14
najjarmalek <najjarmalek3@gmail.com>
2025-08-14 13:45:11 +0100

    saving orders inngest

--- app/api/order/create/route.js
+++ app/api/order/create/route.js
@@ -1,4 +1,6 @@
+import { inngest } from "@/config/inngest";
 import Product from "@/models/Product";
+import User from "@/models/User";
 import { getAuth } from "@clerk/nextjs/dist/types/server";
 import { NextResponse } from "next/server";
 
@@ -15,7 +17,26 @@ export async function POST(request){
             const product =await Product.findById(item.prodduct)
             return acc + product.offerPrice * item.quantity 
         },0 )
+
+        await inngest.send({
+            name:'order/created',
+            data:{
+                userId,
+                address,
+                items,
+                amount:amount+ Math.floor(amount *0.02),
+                date:Date.now()
+            }
+        })
+
+        const user =await User.findById(userId)
+        user.cartItems={}
+        await user.save()
+
+        return NextResponse.json({success:true,message:'Order Placed'})
+
     } catch (error) {
-        
+        console.log(error)
+        return NextResponse.json({success:false,message:error.message})
     }
 }
